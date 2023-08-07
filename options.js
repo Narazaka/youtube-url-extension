54ce63484a5728604d637478a4cd4baf6a8d6fff
Narazaka <info@narazaka.net>
2023-08-08 00:23:25 +0900

    プレフィクスサイト対応

--- options.js
+++ options.js
@@ -15,28 +15,48 @@ async function loadOptions() {
   });
 }
 
-const jinnaiPrefixUrl = "https://vrc.kuroneko6423.com/proxy?url=";
+const jinnaiPrefixUrls = [
+  { name: "KuronekoServer VRChat Video System", value: "https://kvvs.net/proxy?url=" },
+  { name: "YamaPlayer Questユーザ向けの動画再生解決システム", value: "https://api.yamachan.moe/proxy?url=" },
+  { name: "YTQ System", value: "https://yt.8uro.net/r?v=" },
+  { name: "VRC Video URL Resolve System for Quest", value: "https://qst.akakitune87.net/q?url=" },
+  { name: "VRChat Profile video quest", value: "https://vq.vrcprofile.com/?url=" },
+  { name: "YT2.QUEST", value: "https://yt2.quest/?url=" },
+];
 
 document.addEventListener("DOMContentLoaded", async function () {
   const $prefixUrl = /** @type {HTMLInputElement} */ (
     document.querySelector('[name="prefixUrl"]')
   );
-  const $useJinnai = /** @type {HTMLButtonElement} */ (
-    document.querySelector('[name="useJinnai"]')
+  const $useJinnai = /** @type {HTMLDivElement} */ (
+    document.querySelector('.useJinnai')
   );
+  const buttons = [];
+  for (let i = 0; i < jinnaiPrefixUrls.length; i++) {
+    const jinnaiPrefixUrl = jinnaiPrefixUrls[i];
+    const $button = document.createElement("button");
+    $button.type = "button";
+    $button.textContent = `${jinnaiPrefixUrl.name} を使う`;
+    $button.style.display = "block";
+    $button.addEventListener("click", () => {
+      $prefixUrl.value = jinnaiPrefixUrl.value;
+      onChangeValue();
+    });
+    $useJinnai.appendChild($button);
+    buttons.push($button);
+  }
 
   const options = await loadOptions();
   $prefixUrl.value = options.prefixUrl;
 
   function onChangeValue() {
     chrome.storage.local.set({ prefixUrl: $prefixUrl.value });
-    $useJinnai.disabled = $prefixUrl.value === jinnaiPrefixUrl;
+    for (let i = 0; i < buttons.length; i++) {
+      const $button = buttons[i];
+      $button.disabled = $prefixUrl.value === jinnaiPrefixUrls[i].value;
+    }
   }
 
-  $useJinnai.addEventListener("click", () => {
-    $prefixUrl.value = jinnaiPrefixUrl;
-    onChangeValue();
-  });
   $prefixUrl.addEventListener("input", onChangeValue);
   $prefixUrl.addEventListener("change", onChangeValue);
   onChangeValue();
